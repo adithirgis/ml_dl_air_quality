bc8c4fcd8896eaaf64f4f5b40709e229fbce420b
adithirgis <adithi@ilklabs.com>
2021-12-20 10:56:39 +0530

    commit

--- R/GAM.R
+++ R/GAM.R
@@ -13,3 +13,8 @@ write.csv(file_shared, "gam_model.csv")
 
 write.csv(as.data.frame(gam_model$pred), "gam_cv.csv")
 
+gam_model <- gam(PM2.5 ~ s(CWV) + s(ELV) + s(AOD) + s(Temp) + s(RH) + s(NDVI) + s(WD) + s(WS) + s(BLH) + s(Press), 
+                   data = file_shared)
+file_shared$model_pred2 <- predict(gam_model, newdata = file_shared)
+plot.gam(gam_model, pages = 1, residuals = TRUE, all.terms = TRUE, 
+     shade = TRUE, shade.col = 2)
