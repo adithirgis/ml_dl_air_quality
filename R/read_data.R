7790c3135da48ba2600f1c38527b20daa5193082
adithirgis <adithi@ilklabs.com>
2022-03-07 18:39:31 +0530

    update

--- R/read_data.R
+++ R/read_data.R
@@ -7,18 +7,21 @@ library(here)
 library(mgcv)
 library(caret)
 library(data.table)
+library(janitor)
 
 set.seed(108)
-
+lat_lon <- read_excel(here("data", "Delhi_sites.xlsx"), sheet = 1) %>% 
+  select("Station_code" = `Station Code`, "lon" = `Longitude (Degree E)`, "lat" = `Latitude (Degree N)`)
 file_shared <- read_excel(here("data", "Final_Delhi_2019_Data.xlsx"), sheet = 1) %>%
   select("Station_code" = StationCode_2019, "CWV" = CWVDailyMean_2019, "ELV" = Elevation_2019, 
          "AOD" = Corrected_DailyMeanAOD_2019, "PM2.5" = Corrected_PM25DailyMean_2019,
          "Temp" = TempDailyMean_2019, "RH" = RHDailyMean_2019, "NDVI" = NDVI_2019,
          "WD" = WDDailyMean_2019, "WS" = WSDailyMean_2019, "BLH" = BLHDailyMean_2019,
          "Press" = PressureDailyMean_2019, "season" = Season_2019, "day" = JulianDay_2019, "month" = Month_2019) %>%
-  mutate_at(c("CWV", "ELV", "AOD", "PM2.5", "Temp", "RH", "NDVI", "WD", "WS", "BLH", "Press", "month"), as.numeric) %>% 
   filter(!is.nan(PM2.5)) %>% 
-  na.omit()
+  left_join(., lat_lon, by = "Station_code") %>% 
+  mutate_at(c("CWV", "ELV", "AOD", "PM2.5", "Temp", "RH", "NDVI", "WD", "WS", "BLH", "Press", "month", "lat", "lon"), as.numeric) %>% 
+  na.omit() 
 
 
 file_shared$Station_code <- as.factor(file_shared$Station_code)
@@ -50,7 +53,7 @@ search_criteria <- list(strategy = "RandomDiscrete",
                         max_runtime_secs = 90 * 60,
                         seed = 108)
 
-
+# This data  is avaiable in the dropbox alone, not on github 
 dir <- here::here("data/Predictors_Spatial_Mapping")
 all_tables <- data.frame()
 list_csv <- list.files(dir, pattern = "\\.csv$")
@@ -70,14 +73,15 @@ names(all_tables) <- gsub("_V", "_", names(all_tables))
 number_of_days <- ncol(table)
 all_tables <- all_tables[-1, ] 
 all_tables <- cbind(all_tables, lat, lon)
-file_meta <- read_csv(here::here("data", "file_meta.csv")) %>% 
-  select("day" = Julain_day, "season" = season) %>% 
+file_meta <- read_excel(here::here("data", "Julian_Season.xlsx")) %>% 
+  select("day" = `Julian Days`, "season" = Season) %>% 
   arrange(day)
 predict_daily <- function(number_of_days, all_tables, model_input_sp, model) {
   for(i in 1:number_of_days) {
     all_tables_sub <- all_tables %>% 
       select(ends_with(paste0("_", as.character(i))), lat, lon) %>% 
-      mutate(day = file_meta[i, "day"], season = file_meta[i, "season"])
+      janitor::remove_empty("rows") %>% 
+      mutate(day = as.character(file_meta[i, "day"]), season = as.character(file_meta[i, "season"]))
     names(all_tables_sub) <- c("WS", "WD", "Temp", "RH", "Press", "NDVI", "ELV",
                                "CWV", "BLH", "AOD", "lat", "lon", "day", "season")
     all_tables_sub <- as.h2o(all_tables_sub)
